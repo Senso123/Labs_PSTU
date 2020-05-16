3bffe26de808ceee42f027f765ba197268a40121
Савченко Антон Николаевич АСУ19-1БЗУ <65408664+Senso123@users.noreply.github.com>
2020-05-17 01:56:39 +0500

    Add files via upload

---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,33 @@
+﻿#include "Time.h" 
+#include <iostream> 
+using namespace std;
+
+void main()
+{
+	
+	Time a;//конструктор без параметров 
+	Time b; //конструктор без параметров 
+	Time c; //конструктор без параметров 
+	Time min;
+	Time sec;
+
+
+	cin>>a;//ввод переменной 
+	cin>>b;//ввод переменной
+
+	//++a;//префиксная операция инкремент 
+	
+	c = a + b;//сложение и постфиксная операция инкремент 
+
+	cout << "vremya1: " << a << endl;//вывод переменной
+
+	cout<<"vremya2: "<<b<<endl; //вывод переменной 
+
+	
+    cout<<"pribavlenie sekund: "<<c<<endl; //вывод переменной
+
+	//cout<<"sravnenie: " <<endl; //вывод переменной 
+	
+	
+}
+
