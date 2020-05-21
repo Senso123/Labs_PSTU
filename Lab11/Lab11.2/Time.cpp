a0b6cf81a66f424737a1738c3138c4963d5e3156
Савченко Антон Николаевич АСУ19-1БЗУ <65408664+Senso123@users.noreply.github.com>
2020-05-21 12:41:09 +0500

    Добавить файлы через загрузку

---
+++ Lab11/Lab11.2/Time.cpp
@@ -0,0 +1,105 @@
+#include "Time.h"
+#include <iostream>
+using namespace std;
+
+//перегрузка операции присваивания
+Time&Time::operator=(const Time&t)
+{
+	//проверка на самоприсваивание
+	if (&t == this) return *this;
+	min = t.min;
+	sec = t.sec;
+	return *this;
+}
+
+//перегрузка глобальной функции-операции ввода
+istream&operator>>(istream&in, Time&t)
+{
+	cout << "min?"; in >> t.min;
+	cout << "sec?"; in >> t.sec;
+	return in;
+}
+
+//перегрузка глобальной функции-операции вывода
+ostream&operator<<(ostream&out, const Time&t)
+{
+	return (out << t.min << ":" << t.sec);
+}
+
+bool Time::operator <(const Time &t) const
+{
+	if (min < t.min)return true;
+	if (min == t.min&&sec<t.sec)return true;
+	return false;
+}
+
+bool Time::operator >(const Time &t) const
+{
+	if (min > t.min)return true;
+	if (min == t.min && sec > t.sec) return true;
+	return false;
+}
+
+bool Time::operator<=(const Time &t)
+{
+	int temp1 = min * 60 + sec;
+	int temp2 = t.min * 60 + t.sec;
+	return (temp1 <= temp2);
+
+}
+
+bool Time::operator >=(const Time &t) const
+{
+	int temp1 = min * 60 + sec;
+	int temp2 = t.min * 60 + t.sec;
+	return (temp1 >= temp2);
+}
+
+bool Time::operator ==(const Time &t) const
+{
+	int temp1 = min * 60 + sec;
+	int temp2 = t.min * 60 + t.sec;
+	return (temp1 == temp2);
+}
+
+Time Time::operator+(const Time&t)
+{
+	int temp1 = min * 60 + sec;
+	int temp2 = t.min * 60 + t.sec;
+	Time p;
+	p.min = (temp1 + temp2) / 60;
+	p.sec = (temp1 + temp2) % 60;
+	return p;
+}
+
+//перегрузка бинарной операции деления Time/Time
+Time Time::operator/(const Time&t)
+{
+	int temp1 = min * 60 + sec;
+	int temp2 = t.min * 60 + t.sec;
+	Time p;
+	p.min = (temp1 / temp2) / 60;
+	p.sec = (temp1 / temp2) % 60;
+	return p;
+}
+//перегрузка бинарной операции деления Time на число
+Time Time::operator/(const int&t)
+{
+	int temp1 = min * 60 + sec;
+	Time p;
+	p.min = (temp1 / t) / 60;
+	p.sec = (temp1 / t) % 60;
+	return p;
+}
+
+//перегрузка бинарной операции умножения на Time
+Time Time::operator*(const Time&t)
+{
+	int temp1 = min * 60 + sec;
+	int tempT = t.min * 60 + t.sec;
+	int rez = temp1 * tempT;
+	Time p;
+	p.min = rez / 60;
+	p.sec = rez % 60;
+	return p;
+}
